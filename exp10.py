cfd758a56204b682cae3415a5bc2aabeae2164a2
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:15:45 +0530

    Create exp10.py

---
+++ exp10.py
@@ -0,0 +1,8 @@
+"""
+============================
+To perform color transform, RGB to HSI or HSI to RGB, on raw satellite image
+or image with general formats.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
