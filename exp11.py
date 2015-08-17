7d2d322e8251c94f4437610639579900e4c4f90b
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:17:06 +0530

    Create exp11.py

---
+++ exp11.py
@@ -0,0 +1,7 @@
+"""
+============================
+To perform supervised image classification using Maximum Likelihood Classification Technique.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
