2de43dc362b7e9b879f015376f5517e6b7c1fd34
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:20:13 +0530

    Create exp13.py

---
+++ exp13.py
@@ -0,0 +1,8 @@
+"""
+============================
+To perform accuracy assessment on classified images and get overall, producer and user 
+accuracies with Kappa coefficent.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
