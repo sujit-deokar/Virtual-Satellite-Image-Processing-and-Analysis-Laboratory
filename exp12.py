1701f0a88e8e4a33f63bcd2b71e18050f8802514
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:18:14 +0530

    Create exp12.py

---
+++ exp12.py
@@ -0,0 +1,7 @@
+"""
+============================
+To perform supervised image classification using Artifical Neural Networks.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
