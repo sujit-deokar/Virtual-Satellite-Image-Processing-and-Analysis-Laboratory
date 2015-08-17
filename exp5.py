661d4f54177c818cb2b15bb6c2e9cb68393ea9a2
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 11:59:29 +0530

    Create exp5.py

---
+++ exp5.py
@@ -0,0 +1,9 @@
+"""
+============================
+To genearate edge magnitude image from the corrosponding input raw satellite image or image 
+with general formats through various edge operators such as Robert operator, Sobel operator, 
+Perwitt operator, etc.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
