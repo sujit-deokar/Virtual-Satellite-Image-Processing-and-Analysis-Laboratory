0503bd67f9b568cf1e6cec47566cb1a501773785
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:13:30 +0530

    Create exp9.py

---
+++ exp9.py
@@ -0,0 +1,8 @@
+"""
+============================
+To apply K-mean clusttering with user defined class and maximum number of iteration
+on the input raw satellite image or image with general formats.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
