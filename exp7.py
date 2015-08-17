ded7f620fa1384e64fdd876f6021a8de519e5699
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:03:54 +0530

    Create exp7.py

---
+++ exp7.py
@@ -0,0 +1,8 @@
+"""
+============================
+To apply various Morphological operations, such as Erosion, Dilation, Boundary, Detection, Top-hat Transform, etc, 
+on specific bands of input raw satellite image or image with general formats.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
