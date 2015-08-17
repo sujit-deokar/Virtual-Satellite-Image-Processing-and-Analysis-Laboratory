552320a0ed42f8545db82769255dc99aaf39a444
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 11:47:53 +0530

    Delete exp2.py

--- exp2.py-To-view-single-band-or-color-composite-image.-Subset-image-and-its-pixel-data-of-raw-satellite-image-or-general-image-formats-with-image-histogram.-created-17/08/exp2.py
+++
@@ -1,8 +0,0 @@
-"""
-============================
-To Enhance the contrast of input raw satellite image or image with general fromats through various 
-enhancement methods such as Linear Streching, enhancement using Standard Deviation, Histogram Equalization, etc.
-============================
-created: 17/08/2015
-auhor: sujitdeokar30@gmail.com
-"""
