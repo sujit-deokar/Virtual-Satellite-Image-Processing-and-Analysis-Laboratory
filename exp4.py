987c9528393a1cba146ba34f140eb8d71a379444
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 11:56:57 +0530

    Create exp4.py

---
+++ exp4.py
@@ -0,0 +1,8 @@
+"""
+============================
+To genarate principle components of input raw image with general formats 
+and also get inverse PCA with required number of principle components.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
