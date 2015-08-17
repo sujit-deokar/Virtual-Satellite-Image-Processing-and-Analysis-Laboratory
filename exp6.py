05465d7d7ba60ca6c3d5f0590c872e1793a17d39
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:01:25 +0530

    Create exp6.py

---
+++ exp6.py
@@ -0,0 +1,8 @@
+"""
+============================
+To get Frequency spectrum of input raw satellite image or image with general formats, 
+apply Gaussian and Butterworth filtering to the same.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
