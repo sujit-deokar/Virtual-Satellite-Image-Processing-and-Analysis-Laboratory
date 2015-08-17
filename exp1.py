3b306fba03bc2bd271f4099c0108564e6d311612
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 11:24:43 +0530

    Create exp1.py

---
+++ exp1.py
@@ -0,0 +1,10 @@
+
+"""
+============================
+To view single band or color composite image. Subset image and its pixel data of raw satellite image
+or general image formats with image histogram.
+============================
+
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
