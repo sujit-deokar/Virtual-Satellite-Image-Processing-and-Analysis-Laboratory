04c3ea87d8ee024b3d3db0788142670dcc8b54ea
Sujit Deokar <sujitdeokar@ymail.com>
2015-08-17 12:11:37 +0530

    Create exp8.py

---
+++ exp8.py
@@ -0,0 +1,8 @@
+"""
+============================
+To apply band arithmatic and get standard indices, such as Normalised Vegetion Index,
+Soil Adjusted Vegetation Index, etc on raw input satellite image or image with general formats.
+============================
+created: 17/08/2015
+auhor: sujitdeokar30@gmail.com
+"""
