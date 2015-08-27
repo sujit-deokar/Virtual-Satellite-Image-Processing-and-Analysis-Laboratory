a28b7859d0634bfb5a7ca9c3aea049929bf27596
Sujit Deokar <sujitdeokar30@gmail.com>
2015-08-27 15:59:47 +0530

    Update exp1.py

--- exp1.py
+++ exp1.py
@@ -10,4 +10,25 @@ auhor: sujitdeokar30@gmail.com
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2, cv
+import cv2
+
+Color_image = cv2.imread('sample.jpg')
+
+b,g,r = cv2.split(Color_image)      
+New_color_image = cv2.merge([r,g,b])     
+
+plt.imshow(Color_image)
+plt.show()
+
+plt.imshow(b)
+plt.show()
+
+plt.imshow(New_color_image)
+plt.show()
+
+color = ('b','g','r')
+for i,col in enumerate(color):
+    histr = cv2.calcHist([Color_image],[i],None,[256],[0,256])
+    plt.plot(histr,color = col)
+    plt.xlim([0,256])
+plt.show()
