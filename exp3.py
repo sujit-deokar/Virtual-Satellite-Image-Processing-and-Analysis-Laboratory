fdf50da4d51bf91c0b9f0e70f724e12eba12aa40
Sujit Deokar <sujitdeokar30@gmail.com>
2015-10-21 15:42:04 +0530

    Update exp3.py

--- exp3.py
+++ exp3.py
@@ -6,3 +6,29 @@ smoothing filters such as average filter, Weighted average filter, Gaussian smoo
 created: 17/08/2015
 auhor: sujitdeokar30@gmail.com
 """
+from scipy import misc, signal
+import numpy as np
+
+x = misc.lena()
+x1 = misc.lena().astype(np.float32)
+w = signal.gaussian(50, 5.0)
+laplacian = np.array([[0,1,0], [1,-4,1], [0,1,0]], dtype=np.float32)
+derfilt = np.array([1.0, -2, 1.0], dtype=np.float32)
+ck = signal.cspline2d(x1, 8.0)
+deriv = (signal.sepfir2d(ck, derfilt, [1]) + signal.sepfir2d(ck, [1], derfilt))
+im1 = signal.convolve2d(x1,laplacian,mode='same',boundary='symm')
+im2 = signal.sepfir2d(x, w, w) 
+plt.subplot(221)
+plt.imshow(x)
+plt.title('original image')
+plt.subplot(222)
+plt.imshow(im1)
+plt.title('laplacian filter image')
+plt.subplot(223)
+plt.imshow(im2)
+plt.title('gaussian blurred iamge')
+plt.subplot(224)
+plt.imshow(deriv)
+plt.gray()
+plt.title('spline edge filter iamge')
+plt.show()
