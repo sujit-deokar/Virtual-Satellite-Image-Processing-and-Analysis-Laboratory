a9495852dbf4baa6aae0ce53fca76b99308b1385
Sujit Deokar <sujitdeokar30@gmail.com>
2015-10-23 11:23:09 +0530

    Update exp4.py

--- exp4.py
+++ exp4.py
@@ -6,3 +6,24 @@ and also get inverse PCA with required number of principle components.
 created: 17/08/2015
 auhor: sujitdeokar30@gmail.com
 """
+import numpy as np
+from sklearn.decomposition import PCA
+from scipy import misc
+import pylab as plt
+
+X = misc.lena()
+pca = PCA(n_components=50, whiten=False)
+data = pca.fit_transform(X)
+PCA(copy=True, n_components=2, whiten=False)
+print(pca.explained_variance_ratio_) 
+new_data = pca.inverse_transform(data)
+shape(new_data)
+
+plt.subplot(121)
+plt.imshow(X)
+plt.title('Orignal image')
+plt.subplot(122)
+plt.imshow(new_data)
+plt.title('Reconstructed image')
+plt.gray()
+plt.show()
