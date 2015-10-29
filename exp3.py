40663431cf91546b1ebbf16053ab0d2f1de1e073
Sujit Deokar <sujitdeokar30@gmail.com>
2015-10-29 12:10:50 +0530

    Update exp3.py

--- exp3.py
+++ exp3.py
@@ -1,11 +1,11 @@
-"""
+
 ============================
 **To smoothen the input raw satellite image or image with general formats using various 
 smoothing filters such as average filter, Weighted average filter, Gaussian smoothing, etc.**
 ============================
 created: 17/08/2015
 *auhor: sujitdeokar30@gmail.com*
-"""
+
 from scipy import misc, signal
 import numpy as np
 
