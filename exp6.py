6e67bf7cb1132c8543fb09a9905213ebbf7594e4
Sujit Deokar <sujitdeokar30@gmail.com>
2015-11-05 16:41:01 +0530

    Update exp6.py

--- exp6.py
+++ exp6.py
@@ -6,3 +6,7 @@ apply Gaussian and Butterworth filtering to the same.
 created: 17/08/2015
 auhor: sujitdeokar30@gmail.com
 """
+import numpy as np
+import numpy.fft as fft
+from scipy import misc, signal
+import matplotlib.pyplot as plt
