068c1b1507e399c3ea5814ac5e0c88af0b7106d1
Sujit Deokar <sujitdeokar30@gmail.com>
2015-11-27 14:48:30 +0530

    Update exp6.py

--- exp6.py
+++ exp6.py
@@ -10,3 +10,19 @@ import numpy as np
 import numpy.fft as fft
 from scipy import misc, signal
 import matplotlib.pyplot as plt
+
+img = misc.lena()
+w = signal.gaussian(50, 5.0)
+fft_image = fft.fftshift(fft2(img))
+real_fft = np.real(fft_image)
+im2 = signal.sepfir2d(img, w, w)
+fft_out = fft.fftshift(fft2(im2))
+real_out_fft = np.real(fft_out)
+plt.figure()
+plt.subplot(121)
+plt.plot(real_fft)
+plt.title('FFT of original image')
+plt.subplot(122)
+plt.plot(real_out_fft)
+plt.title('FFT of filtered image')
+plt.show()
