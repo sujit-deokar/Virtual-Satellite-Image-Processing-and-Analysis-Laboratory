14214978ab1ea93187e7dcc098fc2a72b57dad40
Sujit Deokar <sujitdeokar30@gmail.com>
2015-11-30 15:24:20 +0530

    Update exp7.py

--- exp7.py
+++ exp7.py
@@ -6,3 +6,13 @@ on specific bands of input raw satellite image or image with general formats.
 created: 17/08/2015
 auhor: sujitdeokar30@gmail.com
 """
+from PIL import Image 
+from scipy import misc, ndimage
+import pylab as plt
+
+'''
+Binary closing operation using scipy.ndimage library code is referenced from scipy documentation
+'''
+a = np.zeros((5,5), dtype=np.int)
+a[1:-1, 1:-1] = 1; a[2,2] = 0
+print a
